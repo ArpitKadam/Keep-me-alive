4e581f4c39fb9201b63da0f27d3196ce738e8a2c
Arpit Kadam <arpitkadam922@gmail.com>
2025-08-11 03:41:28 +0530

    second commit

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import datetime
 def ping_backend():
     urls = [
         ("Web App", "https://hackrx-insurance-app-1.onrender.com"),
-        ("API Health", "https://hackrx-insurance-app.onrender.com/health")
+        ("API Health", "https://hackrx-insurance-app.onrender.com/health"),
+        ("Chest Cancer App", "https://chest-cancer-app.onrender.com"),
+        ("Stock Prediction App", "https://stock-predictor-app-42bo.onrender.com"),
+        ("Book Recommender App", "https://book-recommender-app-rs9r.onrender.com"),
+        ("Book Recommender App", "https://book-recommender-app2.streamlit.app/"),
+        ("GAN Image Generator", "https://gan-image-generator.streamlit.app/"),
+        ("Resume Screening App", "https://resume-screening-app-ppqfi6sscp4vm3nl7tjwko.streamlit.app/")
     ]
     
     for name, url in urls:
